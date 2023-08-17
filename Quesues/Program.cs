b89c57604d58e034d8b90842e3ec258549a78fea
LungeloTZungu <lzungu56@gmail.com>
2023-08-17 17:42:22 +0200

    Add project files.

---
+++ Quesues/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Quesues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue queue = new Queue(10);
+
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            queue.Enqueue(5);
+            // see the queue of people
+            Console.WriteLine("People in Line waiting for orders in the form of numbers : ");
+            foreach(var i in queue.queueArray)
+            {
+                Console.WriteLine(i);
+            }
+
+            // removing from the queue
+            queue.Dequeue();
+            queue.Dequeue();
+       
+          
+            Console.WriteLine("Person in front: ");
+
+            Console.WriteLine(queue.Peek());
+        }
+    }
+}
---
+++ Quesues/Queue.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quesues
+{
+   public  class Queue
+    {
+        public int MazSize  { get; set; } // sets the number of elements beacuse this is an array
+
+        public int[] queueArray { get; set; } // Actual array to store data
+
+        public int Front { get; set; }  // Index to keep track of front
+
+        public int Rear { get; set; } // Index to keep track of adds
+        public int NItems { get; set; } // this wil keep track of length
+
+
+        public Queue(int size)
+        {
+            this.MazSize = size;
+            this.queueArray = new int[size];
+            Front = 0;
+            Rear = -1; // we make this  -1 because there is no item in the array to be considerd the last item
+        }
+
+        public void Enqueue(int item) // inserting to queue
+        {
+            // increment our pointer 
+            Rear++;
+            // insert to there the rear was incremented
+            queueArray[Rear] = item;
+            // increment length
+            NItems++;
+        }
+
+        public int Dequeue()
+        {
+            int temp = queueArray[Front];
+            Front++;
+            if(Front == MazSize)
+            {
+                Front = 0;
+            }
+            NItems--;
+            return temp;
+        }
+        public int Peek()
+        {
+            return queueArray[Front];
+        }
+    }
+}
